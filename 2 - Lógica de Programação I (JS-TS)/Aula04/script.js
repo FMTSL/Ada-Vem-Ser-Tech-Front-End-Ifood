3b58860765dc713aa519cc7a7ee49d33b2e1be1a
FMTSL <Felipe_Matos@hotmail.com.br>
2023-11-23 10:59:45 -0300

    atividade

---
+++ 2 - Lógica de Programação I (JS-TS)/Aula04/script.js
@@ -0,0 +1,43 @@
+//Resolução de Exercicios Aula 03
+//Criação de Objetos
+// const parceiro = [
+//     { parceirosId: '19660156627897', nome: 'Fernanda Santos' },
+//     { parceirosId: '23998058019370', nome: 'Rafael Souza' },
+//     { parceirosId: '92291338611', nome: 'Maria Silva' },
+//     { parceirosId: '55443795656', nome: 'Maria Souza' },
+//     { parceirosId: '77743761456', nome: 'Ana Costa' },
+//     { parceirosId: '47202302326', nome: 'Maria Ferreira' },
+//     { parceirosId: '58017232567', nome: 'Sofia Costa' },
+//     { parceirosId: '16733009491247', nome: 'Lucas Silva' },
+//     { parceirosId: '63351859919', nome: 'Rafael Souza' },
+//     { parceirosId: '84297701780', nome: 'Carlos Oliveira' }
+// ]
+
+
+//Exemplo do For para Tabuada
+// let tabuada = 5 
+
+// for (let i = 1; i< 11; i++){
+//     console.log(`${i} x ${tabuada} = ${i* tabuada}`)
+// }
+
+
+// const numero = [12, 13, 14, 15]
+
+// for (let i=0; i < 11; i++){
+//     console.log(numero[i])
+// }
+
+
+//For-in
+
+const aluno = {
+    nome: 'Felipe',
+    idade: '26'
+}
+
+for (let index in numeros){
+    console.log(index)
+}
+
+console.log(aluno.nome)
